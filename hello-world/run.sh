5c2de4f2d0078c6776fa0b98c32cd34da44b6314
Mike Harder <mharder@microsoft.com>
2020-03-17 17:03:57 -0700

    Add sample output

--- hello-world/run.sh
+++ hello-world/run.sh
@@ -1,3 +1,24 @@
 cat << EOF
-Hello, World!
+Application started.
+
+#StartJobStatistics
+{
+    "Metadata": [
+        {
+            "Source": "HelloWorld",
+            "Name": "helloworld/throughput",
+            "ShortDescription": "Throughput (ops/sec)",
+            "LongDescription": "Throughput (ops/sec)",
+            "Format": "n2" // number with two decimals
+        }
+    ],
+    "Measurements": [
+        {
+            "Timestamp": "2020-03-17T20:06:13.9414065Z",
+            "Name": "helloworld/throughput",
+            "Value": 123.456
+        }
+    ]
+}
+#EndJobStatistics
 EOF
